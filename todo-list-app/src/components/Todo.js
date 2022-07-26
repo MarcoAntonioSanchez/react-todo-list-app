05a02b145b4f726193a73b273378d4eba1376564
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-07-25 23:05:00 -0500

    05. Integrating props into the 1st component

---
+++ todo-list-app/src/components/Todo.js
@@ -0,0 +1,25 @@
+// Importando librerias de react para el componente
+import React from "react";
+
+// Exportando y definiendo el componente al mismo tiempo
+// Los componentes siempre deben regresar algo (return)
+export default function Todo(props) {
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
+        <label className="todo-label" htmlFor="todo-0">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">Eat</span>
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">Eat</span>
+        </button>
+      </div>
+    </li>
+  );
+}
