09d641e7d88940de4b91bbbbd8fca7ce08575702
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-07-30 21:57:49 -0500

    17. Adding a conditional to blank inputs on handleSubmit()

--- todo-list-app/src/components/Form.js
+++ todo-list-app/src/components/Form.js
@@ -4,11 +4,19 @@ import React, { useState } from "react";
 
 function Form(props) {
   // Declaring the const props as an array to show and update name prop
-  const [name, setName] = useState("Use Hooks!");
+  const [name, setName] = useState("");
+  function handleChange(e) {
+    setName(e.target.value);
+  }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    props.addTask("Say Hello!");
+    if (name == "") {
+      alert("Can't be an empty input, type something");
+    } else {
+      e.preventDefault();
+      props.addTask(name);
+      setName("");
+    }
   }
 
   return (
@@ -24,6 +32,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
